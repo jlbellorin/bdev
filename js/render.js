427763280f85909fa730bf3de0594f5e862a0287
José Bellorín <jlbellorin10@gmail.com>
2019-09-09 15:08:05 -0400

    Some Updates

--- js/render.js
+++
@@ -1,7 +0,0 @@
-const render = function (template, selector) {
-  const node = document.querySelector(selector);
-  if (!node) return;
-  node.insertAdjacentHTML('afterbegin', template);
-}
-
-export default render;
