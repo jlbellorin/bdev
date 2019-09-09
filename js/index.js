70d8f70a9231041f782c54d49a3db3c8e560091b
José Bellorín <jlbellorin10@gmail.com>
2019-09-09 13:36:20 -0400

    Some Updates

--- js/index.js
+++ js/index.js
@@ -2,32 +2,19 @@ import render from './render.js';
 import projects from './projects.js';
 
 const projectTemp = (project) => `
-<div class="project">
-  <div class="project__side project__side--front" style="background-image: url(${project.bgImg});">
-    &nbsp;
-  </div>
-  <div class="project__side project__side--back">
-    <div class="project__back-grid">
-      <p class="project__title">${project.title}</p>
-      <p class="project__description">${project.desc}</p>
-      <a href="${project.link}" class="btn btn--white">Visit!</a>
+  <div class="project">
+    <div class="project__side project__side--front" style="background-image: url(${project.bgImg});">
+      &nbsp;
+    </div>
+    <div class="project__side project__side--back">
+      <div class="project__back-grid">
+        <p class="project__title">${project.title}</p>
+        <p class="project__description">${project.desc}</p>
+        <a href="${project.link}" class="btn btn--white">Visit!</a>
+      </div>
     </div>
   </div>
-</div>
 `;
 
-function renderMsg() {
-  render(`
-    <div class="message-submitted">
-      <p>Message Submitted!</p>
-    </div>
-  `, 'footer');
-
-  setTimeout(() => {
-    document.querySelector('.message-submitted').style.display = 'none';
-  }, 5000);
-}
-
-document.querySelector('form').addEventListener('submit', renderMsg);
 projects.slice(0, 3).forEach(project => render(projectTemp(project), '#preview-projects'));
 projects.forEach(project => render(projectTemp(project), '#all-projects'));
